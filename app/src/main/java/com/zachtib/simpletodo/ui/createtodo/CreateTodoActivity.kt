c9b8432fdd4499633f02c8ce61504954197ec922
Zach Tibbitts <zachtib@gmail.com>
2021-09-18 10:25:01 -0400

    Add CreateTodoActivity

---
+++ app/src/main/java/com/zachtib/simpletodo/ui/createtodo/CreateTodoActivity.kt
@@ -0,0 +1,61 @@
+package com.zachtib.simpletodo.ui.createtodo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.zachtib.simpletodo.R
+
+class CreateTodoActivity : AppCompatActivity() {
+
+    // A delegate call, just like in TodoListActivity
+    private val viewModel by viewModels<CreateTodoViewModel> {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                // Suppressing a warning, this object will only ever be used to
+                // create CreateTodoViewModels
+                @Suppress("UNCHECKED_CAST")
+                return CreateTodoViewModel() as T
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createedit_todo)
+
+        // As usual, get all of our UI elements with findViewById
+        val editTodoItemHeader = findViewById<TextView>(R.id.editTodoItemHeader)
+        val titleEditText = findViewById<EditText>(R.id.titleEditText)
+        val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        // Set the header, because it's not set in the layout XML
+        editTodoItemHeader.text = getString(R.string.add_a_todo)
+
+        // We observe this value so the viewModel can disable the button
+        // (ie, while saving is in progress)
+        viewModel.saveButtonEnabled.observe(this) { saveButtonEnabled ->
+            saveButton.isEnabled = saveButtonEnabled
+        }
+
+        // Bind the two EditText fields and save button to our ViewModel
+        titleEditText.doAfterTextChanged(viewModel::onTitleChanged)
+        descriptionEditText.doAfterTextChanged(viewModel::onDescriptionChanged)
+        saveButton.setOnClickListener(viewModel::onSavePressed)
+
+        // Observe this value to know when saving is complete.
+        viewModel.saveComplete.observe(this) { saveWasSuccessful ->
+            if (saveWasSuccessful) {
+                // Check that the save was successful, and if so, close the screen
+                finish()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/zachtib/simpletodo/ui/createtodo/CreateTodoViewModel.kt
@@ -0,0 +1,52 @@
+package com.zachtib.simpletodo.ui.createtodo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class CreateTodoViewModel(
+
+) : ViewModel() {
+
+    // Our private, mutable state
+    private val mutableSaveButtonEnabled = MutableLiveData(false)
+    private val mutableSaveComplete = MutableLiveData(false)
+    private var todoItemTitle: String = ""
+    private var todoItemDescription: String = ""
+
+    // Publicly accessible state our activity will use
+    val saveButtonEnabled: LiveData<Boolean> = mutableSaveButtonEnabled
+    val saveComplete: LiveData<Boolean> = mutableSaveComplete
+
+    // These functions take in nullable CharSequences because that is what EditTexts give us,
+    // so the ViewModel will handle converting them back into Strings
+    fun onTitleChanged(newValue: CharSequence?) {
+        // Covert the newValue back to a String, or use empty string if it is null
+        todoItemTitle = newValue?.toString() ?: ""
+
+        // We only want to allow saving if the user has entered some text for title
+        mutableSaveButtonEnabled.value = todoItemTitle.isNotBlank()
+    }
+
+    fun onDescriptionChanged(newValue: CharSequence?) {
+        todoItemDescription = newValue?.toString() ?: ""
+    }
+
+    // Adding an ignored parameter to this function so it matches a
+    // Button's ClickListener
+    fun onSavePressed(ignored: Any? = null) {
+        // Disable the save button while we are performing the save
+        mutableSaveButtonEnabled.value = false
+        viewModelScope.launch {
+            try {
+                // TODO: Perform the save operation
+                mutableSaveComplete.value = true
+            } catch (e: Exception) {
+                // Something went wrong, re-enable the save button
+                mutableSaveButtonEnabled.value = true
+            }
+        }
+    }
+}
