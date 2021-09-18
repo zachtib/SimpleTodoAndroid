af8da9b45c2f8398cccc778a0bcbd5c9a926bdaf
Zach Tibbitts <zachtib@gmail.com>
2021-09-18 10:27:46 -0400

    Cleanup this for now, but it's not being used at the moment

--- app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoActivity.kt
+++ app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoActivity.kt
@@ -1,30 +1,32 @@
 package com.zachtib.simpletodo.ui.edittodo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.zachtib.simpletodo.R
-import com.zachtib.simpletodo.ui.todolist.TodoListViewModel
 
 
-// A constant we'll use when launching this screen in "edit mode"
-const val EDIT_TODO_ITEM_ID = "EDIT_TODO_ITEM_ID"
-
-// We're going to use the same activity for both editing and creating Todos
 class EditTodoActivity : AppCompatActivity() {
+
+    companion object {
+        // A constant we'll use when launching this screen
+        const val EDIT_TODO_ITEM_ID = "EDIT_TODO_ITEM_ID"
+    }
+
+    // Get the id of the item we're editing, or default to -1 if it was not set.
+    private val itemId: Int = intent.getIntExtra(EDIT_TODO_ITEM_ID, -1)
 
     // A delegate call, just like in TodoListActivity
     private val viewModel by viewModels<EditTodoViewModel> {
-
-        // Get the id of the item we're editing, or default to -1 if it was not set.
-        // We'll treat -1 in the ViewModel as us creating a new item, rather than editing.
-        val itemId = intent.getIntExtra(EDIT_TODO_ITEM_ID, -1)
-
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-
                 // Suppressing a warning, this object will only ever be used to
                 // create EditTodoViewModels
                 @Suppress("UNCHECKED_CAST")
@@ -35,6 +37,13 @@ class EditTodoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_todo)
+        setContentView(R.layout.activity_createedit_todo)
+
+        // As usual, get all of our UI elements with findViewById
+        val editTodoItemHeader = findViewById<TextView>(R.id.editTodoItemHeader)
+        val titleEditText = findViewById<EditText>(R.id.titleEditText)
+        val todoItemComplete = findViewById<CheckBox>(R.id.todoItemComplete)
+        val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
     }
 }
--- app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoViewModel.kt
+++ app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoViewModel.kt
@@ -3,7 +3,6 @@ package com.zachtib.simpletodo.ui.edittodo
 import androidx.lifecycle.ViewModel
 
 class EditTodoViewModel(
-    // If set, we are editing an existing item, otherwise we are creating a new one
     val editingTodoItemId: Int
 ) : ViewModel() {
 }
