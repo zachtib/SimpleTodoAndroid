c03f0982a604d4b7da597513cb34a22a5947664d
Zach Tibbitts <zachtib@gmail.com>
2021-09-19 09:49:59 -0400

    Add EditTodoActivity.kt

--- app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoActivity.kt
+++ app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoActivity.kt
@@ -1,16 +1,17 @@
 package com.zachtib.simpletodo.ui.edittodo
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.zachtib.simpletodo.R
+import com.zachtib.simpletodo.data.SimpleTodoDatabase
 
 
 class EditTodoActivity : AppCompatActivity() {
@@ -20,24 +21,35 @@ class EditTodoActivity : AppCompatActivity() {
         const val EDIT_TODO_ITEM_ID = "EDIT_TODO_ITEM_ID"
     }
 
-    // Get the id of the item we're editing, or default to -1 if it was not set.
-    private val itemId: Int = intent.getIntExtra(EDIT_TODO_ITEM_ID, -1)
+    // Get the id of the item we're editing, or -1 if it was not set.
+    private val itemId: Int
+        get() = intent.getIntExtra(EDIT_TODO_ITEM_ID, -1)
 
     // A delegate call, just like in TodoListActivity
     private val viewModel by viewModels<EditTodoViewModel> {
+        val database = SimpleTodoDatabase.getInstance(this)
+
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
                 // Suppressing a warning, this object will only ever be used to
                 // create EditTodoViewModels
                 @Suppress("UNCHECKED_CAST")
-                return EditTodoViewModel(itemId) as T
+                return EditTodoViewModel(itemId, database.todoItemDao()) as T
             }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_createedit_todo)
+        setContentView(R.layout.activity_create_todo)
+
+        if (itemId == -1) {
+            // No itemId was set, so there's no way for us
+            // to load anything to edit, so exit.
+            Log.w("EditTodoActivity",
+                "EditTodoActivity was launched without setting EDIT_TODO_ITEM_ID")
+            finish()
+        }
 
         // As usual, get all of our UI elements with findViewById
         val editTodoItemHeader = findViewById<TextView>(R.id.editTodoItemHeader)
@@ -45,5 +57,17 @@ class EditTodoActivity : AppCompatActivity() {
         val todoItemComplete = findViewById<CheckBox>(R.id.todoItemComplete)
         val descriptionEditText = findViewById<EditText>(R.id.descriptionEditText)
         val saveButton = findViewById<Button>(R.id.saveButton)
+
+        viewModel.editingTodoItem.observe(this) { todoItem ->
+            editTodoItemHeader.text = getString(R.string.editing_todo, todoItem.title)
+            titleEditText.setText(todoItem.title)
+            todoItemComplete.isChecked = todoItem.isComplete
+            descriptionEditText.setText(todoItem.description)
+            saveButton.isEnabled = true
+        }
+
+        saveButton.setOnClickListener {
+            viewModel.onSavePressed()
+        }
     }
 }
--- app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoViewModel.kt
+++ app/src/main/java/com/zachtib/simpletodo/ui/edittodo/EditTodoViewModel.kt
@@ -1,8 +1,105 @@
 package com.zachtib.simpletodo.ui.edittodo
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
+import com.zachtib.simpletodo.data.TodoItemDao
+import com.zachtib.simpletodo.models.TodoItem
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class EditTodoViewModel(
-    val editingTodoItemId: Int
+    private val editingTodoItemId: Int,
+    private val dao: TodoItemDao,
 ) : ViewModel() {
+
+    companion object {
+        // We'll use these constants to indicate the result of our editing.
+        const val ITEM_SAVED = 1
+        const val ITEM_DELETED = 2
+    }
+
+    // Our private, mutable state
+    private val mutableSaveButtonEnabled = MutableLiveData(false)
+    private val mutableDeleteButtonEnabled = MutableLiveData(false)
+    private val mutableEditingComplete = MutableLiveData<Int>()
+    private var todoItemTitle: String = ""
+    private var todoItemComplete: Boolean = false
+    private var todoItemDescription: String = ""
+
+    // Publicly accessible state our activity will use
+    val saveButtonEnabled: LiveData<Boolean> = mutableSaveButtonEnabled
+    val deleteButtonEnabled: LiveData<Boolean> = mutableDeleteButtonEnabled
+    val editingComplete: LiveData<Int> = mutableEditingComplete
+
+    // We'll use the liveData builder in order to grab the underlying
+    // TodoItem. Since this is an editing screen, we don't really care
+    // all that much about observing changes to it, we just want to fetch
+    // it once to fill in the values on the form.
+    val editingTodoItem: LiveData<TodoItem> = liveData {
+        val todoItem = dao.getTodoItem(editingTodoItemId)
+        emit(todoItem)
+        // We'll also enable the delete button once loading is complete.
+        mutableDeleteButtonEnabled.value = true
+    }
+
+    fun onTitleChanged(newValue: String) {
+        todoItemTitle = newValue
+
+        // We only want to allow saving if the user has entered some text for title
+        mutableSaveButtonEnabled.value = todoItemTitle.isNotBlank()
+    }
+
+    fun onCompleteChanged(newValue: Boolean) {
+        todoItemComplete = newValue
+    }
+
+    fun onDescriptionChanged(newValue: String) {
+        todoItemDescription = newValue
+    }
+
+    fun onDeleteConfirmed() {
+        // Grab the loaded item, or return early if we don't have one, somehow
+        val todoItem = editingTodoItem.value ?: return
+
+        // Disable our save/delete buttons
+        mutableSaveButtonEnabled.value = false
+        mutableDeleteButtonEnabled.value = false
+
+        viewModelScope.launch {
+            try {
+                dao.delete(todoItem)
+                mutableEditingComplete.value = ITEM_DELETED
+            } catch (e: Exception) {
+                Log.e("EditTodoViewModel", "Error deleting TodoItem", e)
+                mutableSaveButtonEnabled.value = true
+                mutableDeleteButtonEnabled.value = true
+            }
+        }
+    }
+
+    fun onSavePressed() {
+        // Grab the loaded item, or return early if we don't have one, somehow
+        val todoItem = editingTodoItem.value ?: return
+
+        // Update the todoItem with the entered values
+        val updatedTodoItem = todoItem.copy(
+            title = todoItemTitle,
+            isComplete = todoItemComplete,
+            description = todoItemDescription
+        )
+
+        mutableSaveButtonEnabled.value = false
+        mutableDeleteButtonEnabled.value = false
+
+        viewModelScope.launch {
+            try {
+                dao.update(updatedTodoItem)
+                mutableEditingComplete.value = ITEM_SAVED
+            } catch (e: Exception) {
+                Log.e("EditTodoViewModel", "Error saving TodoItem", e)
+                mutableSaveButtonEnabled.value = true
+                mutableDeleteButtonEnabled.value = true
+            }
+        }
+    }
 }
