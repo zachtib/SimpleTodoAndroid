07cc6071ca7a7893dc602366feef7deff0c91b39
Zach Tibbitts <zachtib@gmail.com>
2021-09-19 11:23:12 -0400

    Add a MainActivity to hold fragments

---
+++ app/src/main/java/com/zachtib/simpletodo/ui/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.zachtib.simpletodo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.zachtib.simpletodo.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.main_activity)
