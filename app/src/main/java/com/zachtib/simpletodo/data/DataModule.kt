c8692ea788c6dd066cbef9d85020b8082bd59cea
Zach Tibbitts <zachtib@gmail.com>
2021-09-19 11:29:24 -0400

    Migrate Database initialization into Dagger

---
+++ app/src/main/java/com/zachtib/simpletodo/data/DataModule.kt
@@ -0,0 +1,28 @@
+package com.zachtib.simpletodo.data
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DataModule {
+
+    @Singleton
+    @Provides
+    fun provideSimpleTodoDatabase(
+        @ApplicationContext context: Context,
+    ): SimpleTodoDatabase {
+        return Room.databaseBuilder(
+            context, SimpleTodoDatabase::class.java, "todo-db"
+        ).build()
+    }
+
+    @Provides
+    fun provideTodoItemDao(db: SimpleTodoDatabase) = db.todoItemDao()
+}
--- app/src/main/java/com/zachtib/simpletodo/data/SimpleTodoDatabase.kt
+++ app/src/main/java/com/zachtib/simpletodo/data/SimpleTodoDatabase.kt
@@ -1,8 +1,6 @@
 package com.zachtib.simpletodo.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.zachtib.simpletodo.models.TodoItem
 
@@ -14,24 +12,4 @@ import com.zachtib.simpletodo.models.TodoItem
 abstract class SimpleTodoDatabase : RoomDatabase() {
 
     abstract fun todoItemDao(): TodoItemDao
-
-    companion object {
-        // Singleton pattern for building the database. Room databases are expensive in
-        // terms of resources, so we want to make sure we only create one. The public
-        // method, getInstance, will return this instance value if it is set, otherwise
-        // it will call createInstance to perform the database initialization.
-        private var instance: SimpleTodoDatabase? = null
-
-        private fun createInstance(context: Context): SimpleTodoDatabase {
-            val newInstance = Room.databaseBuilder(
-                context, SimpleTodoDatabase::class.java, "todo-db"
-            ).build()
-            instance = newInstance
-            return newInstance
-        }
-
-        fun getInstance(context: Context): SimpleTodoDatabase {
-            return instance ?: createInstance(context)
-        }
-    }
 }
